6b5ad1e7dd280038303d4c48c4fa39c307d9429a
Michael Weylandt <michael.weylandt@gmail.com>
2018-09-09 17:37:56 -0500

    Remove cbass.control
    - Move all arguments to CBASS() back to
      main function
    - Improve error handling

--- R/cbass.R
+++ R/cbass.R
@@ -11,8 +11,6 @@
 #'          will not be clustered).
 #' @param verbose Any of the values \code{0}, \code{1}, or \code{2}. Higher values
 #'                correspond to more verbose output while running.
-#' @param control A list containing advanced parameters for the \code{CBASS} algorithm,
-#'                typically created by \code{\link{cbass.control}}.
 #' @param obs_weights One of the following: \itemize{
 #'                    \item A function which, when called with argument \code{X},
 #'                          returns a n-by-n matrix of fusion weights.
@@ -28,8 +26,27 @@
 #'                    }
 #'                    Note that the weights will be renormalized to sum to
 #'                    \eqn{1/\sqrt{p}} internally.
-#' @param ... Additional arguments used to control the behavior of \code{CBASS}; see
-#'            \code{\link{cbass.control}} for details.
+#' @param obs_labels A character vector of length \eqn{n}: observations (row) labels
+#' @param var_labels A character vector of length \eqn{p}: variable (column) labels
+#' @param X.center.global A logical: Should \code{X} be centered globally?
+#'                        \emph{I.e.}, should the global mean of \code{X} be subtracted?
+#' @param rho For advanced users only (not advisable to change): the penalty
+#'            parameter used for the augmented Lagrangian.
+#' @param max.iter An integer: the maximum number of \code{CBASS} iterations to perform.
+#' @param burn.in An integer: the number of initial iterations at a fixed
+#'                (small) value of \eqn{\lambda}
+#' @param alg.type Which \code{CBASS} variant to use. Allowed values are \itemize{
+#'        \item \code{"cbass"} - The standard \code{CBASS} algorithm with \eqn{L2} penalty;
+#'        \item \code{"cbassviz"} - The back-tracking \code{CBASS} algorithm with \eqn{L2} penalty;
+#'        \item \code{"cbassl1"} - The standard \code{CBASS} algorithm with \eqn{L1} penalty; and
+#'        \item \code{"cbassvizl1"} - The back-tracking \code{CBASS} algorithm with \eqn{L1} penalty.}
+#' @param t A number greater than 1: the size of the multiplicative update to
+#'          the cluster fusion regularization parameter (not used by
+#'          back-tracking variants). Typically on the scale of \code{1.005} to \code{1.1}.
+#' @param npcs An integer >= 2. The number of principal components to compute
+#'             for path visualization.
+#' @param ... Unused arguements. An error will be thrown if any unrecognized
+#'            arguments as given.
 #' @return An object of class \code{CBASS} containing the following elements (among others):
 #'         \itemize{
 #'         \item \code{X}: the original data matrix
@@ -57,7 +74,6 @@
 #'         \item \code{var.labels}: a character vector of length \code{p.var} containing
 #'                                  variable (column) labels
 #'         }
-#' @importFrom stats var
 #' @export
 #' @examples
 #' \dontrun{
@@ -76,7 +92,31 @@ CBASS <- function(X,
                                                           phi = "auto",
                                                           dist.method = "euclidean",
                                                           p = 2),
-                  control = NULL) {
+                  obs_labels = rownames(X),
+                  var_labels = colnames(X),
+                  X.center.global = TRUE,
+                  rho = 1.0,
+                  t = 1.01,
+                  max.iter = 1000000L,
+                  burn.in = 50L,
+                  alg.type = c("cbassviz", "cbassvizl1", "cbass", "cbassl1"),
+                  npcs = min(4L, NCOL(X))) {
+
+  ####################
+  ##
+  ## Input validation
+  ##
+  ####################
+
+  dots <- list(...)
+
+  if (length(dots) != 0L) {
+    if (!is.null(names(dots))) {
+      stop("Unknown argument ", sQuote(names(dots)[1L]), " passed to ", sQuote("CBASS."))
+    } else {
+      stop("Unknown ", sQuote("..."), " arguments passed to ", sQuote("CBASS."))
+    }
+  }
 
   if (!is.matrix(X)) {
     warning(sQuote("X"), " should be a matrix, not a " , class(X)[1],
@@ -88,9 +128,6 @@ CBASS <- function(X,
     stop(sQuote("X"), " must be numeric.")
   }
 
-  n.obs <- NROW(X)
-  p.var <- NCOL(X)
-
   if (anyNA(X)) {
     stop(sQuote("CBASS"), " cannot handle missing data.")
   }
@@ -98,6 +135,54 @@ CBASS <- function(X,
   if (!all(is.finite(X))) {
     stop("All elements of ", sQuote("X"), " must be finite.")
   }
+
+  if (!is.logical(X.center.global) || is.na(X.center.global) || (length(X.center.global) != 1L)) {
+    stop(sQuote("X.center.global"), "must be either ", sQuote("TRUE"), " or ", sQuote("FALSE."))
+  }
+
+  if ( (!is_numeric_scalar(rho)) || (rho <= 0)) {
+    stop(sQuote("rho"), "must be a positive scalar (vector of length 1).")
+  }
+
+  if ( (!is_integer_scalar(npcs)) || (npcs < 2) || (npcs > NCOL(X)) ){
+    stop(sQuote("npcs"), " must be an integer scalar between 2 and ", sQuote("NCOL(X)."))
+  }
+
+  if ( (!is_integer_scalar(max.iter)) || (max.iter <= 1L) ) {
+    stop(sQuote("max.iter"), " must be a positive integer scalar and at least 2.")
+  }
+
+  if ( (!is_integer_scalar(burn.in)) || (burn.in <= 0L) || (burn.in >= max.iter) ) {
+    stop(sQuote("burn.in"), " must be a positive integer less than ", sQuote("max.iter."))
+  }
+
+  alg.type <- match.arg(alg.type)
+
+  if ( (!is_numeric_scalar(t)) || (t <= 1) ) {
+    stop(sQuote("t"), " must be a scalar greater than 1.")
+  }
+
+  ## Get row (observation) labels
+  if (is.null(obs_labels)) {
+    obs_labels <- paste0("Obs", seq_len(NROW(X)))
+  }
+
+  if ( length(obs_labels) != NROW(X) ){
+    stop(sQuote("obs_labels"), " must be of length ", sQuote("NROW(X)."))
+  }
+
+  rownames(X) <- obs_labels <- make.names(obs_labels, unique = TRUE)
+
+  ## Get column (variable) labels
+  if (is.null(var_labels)) {
+    var_labels <- paste0("Var", seq_len(NCOL(X)))
+  }
+
+  if ( length(var_labels) != NCOL(X) ){
+    stop(sQuote("var_labels"), " must be of length ", sQuote("NCOL(X)."))
+  }
+
+  colnames(X) <- var_labels <- make.names(var_labels, unique = TRUE)
 
   if (is.logical(verbose)) {
     verbose.basic <- TRUE
@@ -113,54 +198,11 @@ CBASS <- function(X,
     verbose.deep <- FALSE
   }
 
-  internal.control <- cbass.control(...)
-  if (!is.null(control)) {
-    internal.control <- modifyList(internal.control, control)
-  }
+  n.obs <- NROW(X)
+  p.var <- NCOL(X)
 
-  obs.labels <- internal.control$obs.labels
-  var.labels <- internal.control$var.labels
-  X.center.global <- internal.control$X.center.global
-  rho <- internal.control$rho
-  max.iter <- internal.control$max.iter
-  burn.in <- internal.control$burn.in
-  alg.type <- internal.control$alg.type
-  t <- internal.control$t
-  npcs <- internal.control$npcs
-
-  # get labels
-  if (is.null(obs.labels)) {
-    if (!is.null(rownames(X))) {
-      n.labels <- rownames(X)
-    } else {
-      n.labels <- 1:NROW(X)
-    }
-  } else {
-    if (length(obs.labels) == n.obs) {
-      n.labels <- obs.labels
-    } else {
-      stop("obs.labels should be length NROW(X)")
-    }
-  }
-  if (is.null(var.labels)) {
-    if (!is.null(colnames(X))) {
-      p.labels <- colnames(X)
-    } else {
-      p.labels <- 1:NCOL(X)
-    }
-  } else {
-    if (length(var.labels) == p.var) {
-      p.labels <- var.labels
-    } else {
-      stop("var.labels should be length NCOL(X)")
-    }
-  }
-
-  # center and scale
-  colnames(X) <- var.labels
-  rownames(X) <- obs.labels
+  # Preprocess X
   X.orig <- X
-
   if (X.center.global) {
     X <- X - mean(X)
   }
@@ -351,7 +393,7 @@ CBASS <- function(X,
   cbass.cluster.path.row[["clust.path"]] <- clust.path.row
   cbass.cluster.path.row[["clust.path.dups"]] <- clust.path.dups.row
 
-  cbass.dend.row <- CreateDendrogram(cbass.cluster.path.row, p.labels)
+  cbass.dend.row <- CreateDendrogram(cbass.cluster.path.row, var_labels)
 
   ISP(
     sp.path = cbass.sol.path$v.col.zero.inds %>% t(),
@@ -367,7 +409,7 @@ CBASS <- function(X,
   cbass.cluster.path.col[["clust.path"]] <- clust.path.col
   cbass.cluster.path.col[["clust.path.dups"]] <- clust.path.dups.col
 
-  cbass.dend.col <- CreateDendrogram(cbass.cluster.path.col, n.labels)
+  cbass.dend.col <- CreateDendrogram(cbass.cluster.path.col, obs_labels)
 
   cbass.fit <- list(
     X = X.orig,
@@ -384,110 +426,13 @@ CBASS <- function(X,
     alg.type = alg.type,
     t = t,
     X.center.global = X.center.global,
-    obs.labels = n.labels,
-    var.labels = p.labels
+    obs.labels = obs_labels,
+    var.labels = var_labels
   )
 
   class(cbass.fit) <- "CBASS"
 
   return(cbass.fit)
-}
-
-#' Control for \code{CBASS} fits
-#'
-#' Set \code{CBASS} algorithm parameters
-#'
-#' This function constructs a list containing additional arguments to control
-#' the behavior of the \code{CBASS} algorithm. It is typically only used internally
-#' by \code{\link{CBASS}}, but may be useful to advanced users who wish to
-#' construct the \code{control} argument directly.
-#'
-#' Note that all arguments must be passed by name.
-#'
-#' @param obs.labels A character vector of length \eqn{n}: observations (row) labels
-#' @param var.labels A character vector of length \eqn{p}: variable (column) labels
-#' @param X.center.global A logical: Should \code{X} be centered globally?
-#'                        \emph{I.e.}, should the global mean of \code{X} be subtracted?
-#' @param rho For advanced users only (not advisable to change): the penalty
-#'            parameter used for the augmented Lagrangian.
-#' @param max.iter An integer: the maximum number of \code{CBASS} iterations.
-#' @param burn.in An integer: the number of initial iterations at a fixed
-#'                (small) value of \eqn{\lambda}
-#' @param alg.type Which \code{CBASS} variant to use. Allowed values are \itemize{
-#'        \item \code{"cbass"} - The standard \code{CBASS} algorithm with \eqn{L2} penalty;
-#'        \item \code{"cbassviz"} - The back-tracking \code{CBASS} algorithm with \eqn{L2} penalty;
-#'        \item \code{"cbassl1"} - The standard \code{CBASS} algorithm with \eqn{L1} penalty; and
-#'        \item \code{"cbassvizl1"} - The back-tracking \code{CBASS} algorithm with \eqn{L1} penalty.}
-#' @param t A number greater than 1: the size of the multiplicative update to
-#'          the cluster fusion regularization parameter (not used by
-#'          back-tracking variants). Typically on the scale of \code{1.005} to \code{1.1}.
-#' @param npcs An integer >= 2. The number of principal components to compute
-#'             for path visualization.
-#' @param ... Unused arguements. An error will be thrown if any unrecognized
-#'            arguments as given.
-#' @return A list containing the \code{CBASS} algorithm parameters.
-#' @export
-cbass.control <- function(...,
-                          obs.labels = NULL,
-                          var.labels = NULL,
-                          X.center.global = TRUE,
-                          rho = 1,
-                          t = 1.01,
-                          max.iter = as.integer(1e6),
-                          burn.in = as.integer(50),
-                          alg.type = "cbassviz",
-                          npcs = as.integer(4)) {
-
-  dots <- list(...)
-
-  if (length(dots) != 0L) {
-    if (!is.null(names(dots))) {
-      stop("Unknown argument ", sQuote(names(dots)[1L]), " passed to ", sQuote("CBASS."))
-    } else {
-      stop("Unknown ", sQuote("..."), " arguments passed to ", sQuote("CBASS."))
-    }
-  }
-
-  if (!is.logical(X.center.global) || is.na(X.center.global) || (length(X.center.global) != 1L)) {
-    stop(sQuote("X.center.global"), "must be either ", sQuote("TRUE"), " or ", sQuote("FALSE."))
-  }
-
-  if ( (!is_numeric_scalar(rho)) || (rho <= 0)) {
-    stop(sQuote("rho"), "must be a positive scalar (vector of length 1).")
-  }
-
-  if (!is.null(npcs)) {
-    if (!is.integer(npcs) || npcs <= 1L) {
-      stop(sQuote("npcs"), " must be at least 2.")
-    }
-  }
-
-  if ( (!is_integer_scalar(max.iter)) || (max.iter <= 1L) ) {
-    stop(sQuote("max.iter"), " must be a positive integer scalar and at least 2.")
-  }
-
-  if ( (!is_integer_scalar(burn.in)) || (burn.in <= 0L) || (burn.in >= max.iter) ) {
-    stop(sQuote("burn.in"), " must be a positive integer less than ", sQuote("max.iter."))
-  }
-
-  if (alg.type %not.in% c("cbassviz", "cbass", "cbassl1", "cbassvizl1")) {
-    stop("Unrecognized value of ", sQuote("alg.type;"), " see help for allowed values.")
-  }
-
-  if ( (!is_numeric_scalar(t)) || (t <= 1) ) {
-    stop(sQuote("t"), " must be a scalar greater than 1.")
-  }
-
-  list(
-    obs.labels = obs.labels,
-    var.labels = var.labels,
-    X.center.global = X.center.global,
-    rho = rho,
-    t = t,
-    max.iter = max.iter,
-    burn.in = burn.in,
-    alg.type = alg.type
-  )
 }
 
 #' Print \code{CBASS} Results
